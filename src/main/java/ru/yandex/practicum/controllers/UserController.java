b941ed7740eb8605fd207009785917cd4e4d475e
Катя <ek8512@yandex.ru>
2023-04-20 16:27:55 +0300

    "Финальный проект спринта №9"

--- src/main/java/ru/yandex/practicum/controllers/UserController.java
+++ src/main/java/ru/yandex/practicum/controllers/UserController.java
@@ -27,10 +27,10 @@ public class UserController {
     }
 
     @PutMapping
-    public User update(@Valid @RequestBody User user) {
+    public ResponseEntity<User> update(@Valid @RequestBody User user) {
         log.info("Got request to update user {}", user);
         setLoginAsNameIfEmpty(user);
-        return manager.update(user);
+        return ResponseEntity.ok(manager.update(user));
     }
 
     @GetMapping
