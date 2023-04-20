c8b3d84e7cb7b996aba80199237b1546ee38de73
Катя <ek8512@yandex.ru>
2023-04-20 21:38:53 +0300

    "Финальный проект спринта №9"

--- src/main/java/ru/yandex/practicum/model/User.java
+++ src/main/java/ru/yandex/practicum/model/User.java
@@ -6,7 +6,6 @@ import ru.yandex.practicum.annotation.NoSpaces;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
 
